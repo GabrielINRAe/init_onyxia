8de5a072e9a04b27f821a3fa7f6b19d9569e696c
Gabriel <orabonagabriel@gmail.com>
2025-03-18 15:14:17 +0100

    Update onyxia-init.sh

--- onyxia-init.sh
+++ onyxia-init.sh
@@ -2,13 +2,11 @@ GIT_REPO=stage_pnr_dynafor
 git --depth clone https://forgemia.inra.fr/dynafor/dev-test/gabriel-orabona/${GIT_REPO}.git
 chown -R onyxia:users ${GIT_REPO}/
 
-conda create env -f environment.yaml
-conda activate stage
-
-# conda install -y xarray
-# conda install -y rioxarray
-# conda install -y geopandas
-# conda install -y gdal==3.10.1
-# conda install -y holoviews
-# conda install -y hvplot
-# conda install -y sklearn
+conda install -y xarray
+conda install -y rioxarray
+conda install -y geopandas
+conda install -y gdal==3.10.1
+conda install -y holoviews
+conda install -y hvplot
+conda install -y sklearn
+conda install -y natsort
