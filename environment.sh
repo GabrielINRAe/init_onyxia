a2b97d95ba59eda8ae1aa4337a97690c69cd83d5
Gabriel <orabonagabriel@gmail.com>
2026-03-17 15:51:55 +0100

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,5 +1,12 @@
+GIT_TOKEN=AHh14xclHvx7CyI-nZnHy286MQp1Ojcxawk.01.0z0n9h5ds
 GIT_REPO=fosses_talus_detection
-git clone --depth 1 https://gabriel.orabona:AHh14xclHvx7CyI-nZnHy286MQp1Ojcxawk.01.0z0n9h5ds@forge.inrae.fr/dynafor/dev-test/gabriel-orabona/${GIT_REPO}.git
+
+git clone https://gabriel.orabona:${GIT_TOKEN}@forge.inrae.fr/dynafor/dev-test/gabriel-orabona/${GIT_REPO}.git
+
+
+git -C ${GIT_REPO} remote set-branches origin '*'
+git -C ${GIT_REPO} fetch --all
+
 chown -R onyxia:users ${GIT_REPO}/
 
 pip install -r ${GIT_REPO}/requirements.txt
